4ba776f0af9897dd9f4744c6a611103f98d4a716
Taylor Hogan <taylorb.hogan@gmail.com>
2021-06-17 13:00:25 -0700

    Complete user auth testing

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -3,50 +3,6 @@ const { Validator } = require('sequelize');
 const bcrypt = require('bcryptjs');
 
 
-/************************INSTANCE METHODS************************/
-
-User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
-  const { id, username, email } = this; // context will be the User instance
-  return { id, username, email };
-};
-
-User.prototype.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.hashedPassword.toString());
-};
-
-
-/************************STATIC METHODS************************/
-
-// note the scope: uses the currentUser scope to return a User with that id
- User.getCurrentUserById = async function (id) {
-  return await User.scope('currentUser').findByPk(id);
- };
-
- User.login = async function ({ credential, password }) {
-  const { Op } = require('sequelize');
-  const user = await User.scope('loginUser').findOne({
-    where: {
-      [Op.or]: {
-        username: credential,
-        email: credential,
-      },
-    },
-  });
-  if (user && user.validatePassword(password)) {
-    return await User.scope('currentUser').findByPk(user.id);
-  }
-};
-
-User.signup = async function ({ username, email, password }) {
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({
-    username,
-    email,
-    hashedPassword,
-  });
-  return await User.scope('currentUser').findByPk(user.id);
-};
-
 // the defined scopes must be used explicitly when querying
 // e.g. User.scope('currentUser').findByPk(id)
 module.exports = (sequelize, DataTypes) => {
@@ -96,5 +52,50 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function(models) {
     // associations can be defined here
   };
+
+  /************************INSTANCE METHODS************************/
+
+  User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
+    const { id, username, email } = this; // context will be the User instance
+    return { id, username, email };
+  };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
+
+  /************************STATIC METHODS************************/
+
+  // note the scope: uses the currentUser scope to return a User with that id
+  User.getCurrentUserById = async function (id) {
+    return await User.scope('currentUser').findByPk(id);
+  };
+
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        },
+      },
+    });
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    }
+  };
+
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
+
   return User;
 };
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,6 +4,46 @@ router.post('/test', function(req, res) {
   res.json({ requestBody: req.body });
 });
 
+/************************TESTS************************/
+
+// TEST 4 - requireAuth
+// GET /api/require-auth
+// const { requireAuth } = require('../../utils/auth.js');
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// TEST 3 - restoreUser
+// GET /api/restore-user
+// const { restoreUser } = require('../../utils/auth.js');
+// router.get(
+//   '/restore-user',
+//   restoreUser,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// TEST 2 - setTokenCookie
+// GET /api/set-token-cookie
+// const asyncHandler = require('express-async-handler');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+// router.get('/set-token-cookie', asyncHandler(async (req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       },
+//     })
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// TEST 1
 // test fetch request for the browser - entered into browser console to test
 // fetch('/api/test', {
 //   method: "POST",

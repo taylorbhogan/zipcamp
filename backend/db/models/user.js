988f1fbac1d2dbe75502c24f9adff7daaed120c2
Taylor Hogan <taylorb.hogan@gmail.com>
2021-06-17 12:40:52 -0700

    Add user model methods

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -1,9 +1,54 @@
 'use strict';
 const { Validator } = require('sequelize');
+const bcrypt = require('bcryptjs');
+
+
+/************************INSTANCE METHODS************************/
+
+User.prototype.toSafeObject = function() { // remember, this cannot be an arrow function
+  const { id, username, email } = this; // context will be the User instance
+  return { id, username, email };
+};
+
+User.prototype.validatePassword = function (password) {
+  return bcrypt.compareSync(password, this.hashedPassword.toString());
+};
+
+
+/************************STATIC METHODS************************/
+
+// note the scope: uses the currentUser scope to return a User with that id
+ User.getCurrentUserById = async function (id) {
+  return await User.scope('currentUser').findByPk(id);
+ };
+
+ User.login = async function ({ credential, password }) {
+  const { Op } = require('sequelize');
+  const user = await User.scope('loginUser').findOne({
+    where: {
+      [Op.or]: {
+        username: credential,
+        email: credential,
+      },
+    },
+  });
+  if (user && user.validatePassword(password)) {
+    return await User.scope('currentUser').findByPk(user.id);
+  }
+};
+
+User.signup = async function ({ username, email, password }) {
+  const hashedPassword = bcrypt.hashSync(password);
+  const user = await User.create({
+    username,
+    email,
+    hashedPassword,
+  });
+  return await User.scope('currentUser').findByPk(user.id);
+};
 
 // the defined scopes must be used explicitly when querying
 // e.g. User.scope('currentUser').findByPk(id)
-
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: {

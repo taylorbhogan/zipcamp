f22a4420936bcdb6a9d78b4bc7c21905e10c3ca5
Taylor Hogan <taylorb.hogan@gmail.com>
2021-06-17 14:20:10 -0700

    Install packages in frontend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,11 @@ const apiRouter = require('./api');
 
 router.use('/api', apiRouter);
 
-router.get('/hello/world', function(req, res) {
-  // set a cookie on the res with the name of CSRF-TOKEN to the value of the req.csrfToken method's return.
-  res.cookie('XSRF-TOKEN', req.csrfToken());
-  res.send('Hello World!');
-});
+// // for testing
+// router.get('/hello/world', function(req, res) {
+//   // set a cookie on the res with the name of CSRF-TOKEN to the value of the req.csrfToken method's return.
+//   res.cookie('XSRF-TOKEN', req.csrfToken());
+//   res.send('Hello World!');
+// });
 
 module.exports = router;
-
-
-

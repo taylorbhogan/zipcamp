c1131bc57da54e90054a417c94ebd66a0bd1136d
Taylor Hogan <taylorb.hogan@gmail.com>
2021-09-15 16:47:25 -0700

    Hide add tip button from logged out users

--- frontend/src/components/TipAddModal/index.js
+++ frontend/src/components/TipAddModal/index.js
@@ -1,21 +1,24 @@
 import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { Modal } from '../../context/Modal'
 import TipAddForm from "../TipAddForm";
 import styles from './TipAddModal.module.css'
 
 
-function TipAddModal({thisSpotTips}){
+function TipAddModal({thisSpotTips, spot}){
+  const sessionUser = useSelector(state => state.session.user);
+
   const [showTipAddModal, setShowTipAddModal] = useState(false);
 
   return(
     <div className={styles.buttonWrapper}>
-      {/* {(thisSpotTips.length === 0) && (<div className={styles.buttonBackground}></div>)} */}
-      <button
+      {<button
+        hidden={sessionUser && sessionUser.id === spot?.userId ? false : true}
         onClick={() => setShowTipAddModal(true)}
         className={styles.button}
       >
         add a tip
-      </button>
+      </button>}
       {showTipAddModal && (
         <Modal onClose={() => setShowTipAddModal(false)}>
           <TipAddForm setShowTipAddModal={setShowTipAddModal} />

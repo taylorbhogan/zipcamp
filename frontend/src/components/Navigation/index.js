f29196cbed89fc47e2ec9cc9f2bcbe52bf6d2f97
Taylor Hogan <taylorb.hogan@gmail.com>
2021-09-16 10:22:26 -0700

    Improve navbar styling

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,11 +1,9 @@
-// import React, { useState } from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import LoginFormModal from '../LoginFormModal';
 import DemoLoginButton from '../parts/DemoLoginButton';
 import styles from './Navigation.module.css';
-// import * as sessionActions from '../../store/session'
 
 function Navigation({ isLoaded }) {
   const sessionUser = useSelector(state => state.session.user);
@@ -13,22 +11,19 @@ function Navigation({ isLoaded }) {
   let sessionLinks;
   if (sessionUser) {
     sessionLinks = (
-      <div
-        className={styles.welcomeDiv}
-      >
-        <div className={styles.levi}>
+      <div className={styles.welcomeDiv}>
+        <div className={styles.username}>
           Welcome back, {sessionUser.username}.
         </div>
         <ProfileButton user={sessionUser} />
       </div>
-
     );
   } else {
     sessionLinks = (
       <div className={styles.sessionLinksDiv}>
         <LoginFormModal />
         <DemoLoginButton buttonText={'try it out'} />
-        <Link to='/signup' className={styles.signUpLink}>join</Link>
+        <NavLink to='/signup' className={styles.signUpLink}>join</NavLink>
       </div>
     );
   }
@@ -37,45 +32,34 @@ function Navigation({ isLoaded }) {
     <div className={styles.navbarWrapper}>
       <div className={styles.navbar}>
         <div className={styles.navbarLeft}>
-          <Link
-            className={styles.removeUnderline}
-            exact to='/'>
-            <div className={styles.navbarLogo}>
-              zipcamp
-            </div>
-          </Link>
+          <NavLink
+            className={styles.navbarLogo}
+            exact to='/'
+          >
+            zipcamp
+          </NavLink>
           <div className={styles.navLinkList}>
-            <div>
-              <NavLink
-                className={styles.navLink}
-                id={styles.home}
-                activeClassName={styles.navLinkActive}
-                exact to='/'>home</NavLink>
-            </div>
-            <div>
-              <NavLink
-                className={styles.navLink}
-                activeClassName={styles.navLinkActive}
-                exact to='/spots'>spots</NavLink>
-            </div>
-            {/* <div>
-              <NavLink
-                className={styles.navLink}
-                activeClassName={styles.navLinkActive}
-                exact to ='/areas'>public lands</NavLink>
-            </div> */}
-            {/* <div>
-              <NavLink
+            <NavLink
+              className={styles.navLink}
+              id={styles.home}
+              activeClassName={styles.navLinkActive}
+              exact to='/'>home</NavLink>
+            <NavLink
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+              exact to='/spots'>spots</NavLink>
+            <NavLink
+              className={styles.navLink}
+              activeClassName={styles.navLinkActive}
+              exact to='/areas'>public lands</NavLink>
+            {/* <NavLink
                 className={styles.navLink}
                 activeClassName={styles.navLinkActive}
-                exact to ='/users/adventures'>upcoming adventures</NavLink>
-            </div> */}
+                exact to ='/users/adventures'>upcoming adventures</NavLink> */}
           </div>
         </div>
         <div className={styles.navbarRight}>
-          <div className={styles.sessionLinks}>
-            {isLoaded && sessionLinks}
-          </div>
+          {isLoaded && sessionLinks}
         </div>
       </div>
     </div>

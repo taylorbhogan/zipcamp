ec508ed7ffd6e856bcf77e142999dee6dce4c319
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-05 15:35:10 -0700

    Tidy code

--- frontend/src/components/TipBox/index.js
+++ frontend/src/components/TipBox/index.js
@@ -7,7 +7,7 @@ import { Modal } from '../../context/Modal';
 import styles from './TipBox.module.css'
 
 
-function TipBox({tip}){
+function TipBox({ tip }) {
   const sessionUser = useSelector(state => state.session.user);
   const [showModal, setShowModal] = useState(false);
   const history = useHistory();
@@ -20,44 +20,29 @@ function TipBox({tip}){
     }
   }
 
-  return(
-    <div className={styles.tipBoxWrapper}>
-      <div className={styles.tipCardContainer}>
-        {showModal && (
-            <Modal className={'modalCard'} onClose={() => setShowModal(false)}>
-              <TipForm setShowTipForm={setShowModal} tipId={tip.id}/>
-            </Modal>
-          )}
-        <div className={styles.spotBoxContainerLeft}>
-          <div className={styles.spotImageDiv}></div>
-
-        </div>
-        <div className={styles.spotBoxContainerRight}>
-          <div className={styles.locationDiv}>
-            <div className={styles.invisibleEditWrapper}>
-              <button
-                hidden={sessionUser && sessionUser.id === tip?.userId ? false : true}
-                onClick={() => setShowModal(true)}
-                className={styles.invisibleEdit}
-                >
-              Edit
-              </button>
-              <button
-                hidden={sessionUser && sessionUser.id === tip?.userId ? false : true}
-                onClick={() => handleDelete(tip.id)}
-                className={styles.invisibleDelete}
-              >
-              Delete
-              </button>
-            </div>
-            <div className={styles.createdAt}>{tip.createdAt}</div>
-            <div className={styles.username}>{tip.User.username} wrote:</div>
-            {tip.rating && (
-            <div className={styles.rating}>Rating: {tip.rating}</div>)}
-          </div>
-          <div className={styles.spotBlurb}>{tip.text}</div>
-        </div>
+  return (
+    <div className={styles.tipCardContainer}>
+      {showModal && (
+        <Modal className={'modalCard'} onClose={() => setShowModal(false)}>
+          <TipForm setShowTipForm={setShowModal} tipId={tip.id} />
+        </Modal>
+      )}
+      <div className={styles.buttons}>
+        <button
+          hidden={sessionUser && sessionUser.id === tip?.userId ? false : true}
+          onClick={() => setShowModal(true)}
+          className={styles.editButton}
+        >Edit</button>
+        <button
+          hidden={sessionUser && sessionUser.id === tip?.userId ? false : true}
+          onClick={() => handleDelete(tip.id)}
+          className={styles.deleteButton}
+        >Delete</button>
       </div>
+      <div className={styles.createdAt}>{tip.createdAt}</div>
+      <div className={styles.username}>{tip.User.username} wrote:</div>
+      {tip.rating && <div className={styles.rating}>Rating: {tip.rating}</div>}
+      <div className={styles.tipText}>{tip.text}</div>
     </div>
   )
 }

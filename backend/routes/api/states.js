247589b786962f3d6dfc4d841f2731ab092e9eb0
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-30 15:26:48 -0700

    Tidy code

--- backend/routes/api/states.js
+++ backend/routes/api/states.js
@@ -11,11 +11,4 @@ router.get('/', asyncHandler(async (req, res) => {
   res.json(states);
 }))
 
-
-
-// router.get('/', asyncHandler(async (req, res) => {
-//   const spots = await Spot.findAll({include: [Area, State]});
-//   res.json(spots);
-// }))
-
 module.exports = router;

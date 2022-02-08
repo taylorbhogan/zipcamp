a813d30cbaf7f8d04ea05d1fd530253c3612b7cc
Taylor Hogan <taylorb.hogan@gmail.com>
2022-02-07 16:46:37 -0800

    Standardize h2 formatting

--- frontend/src/components/SpotBox/index.js
+++ frontend/src/components/SpotBox/index.js
@@ -1,27 +1,23 @@
-import React from 'react'
-import styles from './SpotBox.module.css'
-import ShowSpotIdButton from '../ShowSpotIdButton';
+import React from "react";
+import styles from "./SpotBox.module.css";
+// import ShowSpotIdButton from "../ShowSpotIdButton";
 
 function SpotBox({ spot }) {
   return (
-    <a
-    href={`/spots/${spot.id}`}
-    className={styles.container}>
+    <a href={`/spots/${spot.id}`} className={styles.container}>
       <div className={styles.containerLeft}>
         <div className={styles.spotImageDiv}>
           <img
             className={styles.spotImage}
-            alt={'a glimpse of this spot'}
+            alt={"a glimpse of this spot"}
             src={spot.SpotImages ? spot.SpotImages[0]?.imgUrl : null}
           />
         </div>
       </div>
       <div className={styles.containerRight}>
         <h1 className={styles.spotName}>{spot.name}</h1>
-        <div className={styles.locationDiv}>
-          <div className={styles.areaName}>{spot.Area?.name}</div>
-          <div className={styles.stateName}>{spot.State?.name}</div>
-        </div>
+        <h2 className={styles.areaName}>{spot.Area?.name}</h2>
+        <div className={styles.stateName}>{spot.State?.name}</div>
         <div className={styles.spotBlurb}>{spot.blurb}</div>
         {/* <ShowSpotIdButton
           buttonText={'Check it out'}
@@ -30,7 +26,7 @@ function SpotBox({ spot }) {
           href={`/spots/${spot.id}`} /> */}
       </div>
     </a>
-  )
+  );
 }
 
 export default SpotBox;

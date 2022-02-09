b4705f8f5d586d8050a036f336776eb1f418ded8
Taylor Hogan <taylorb.hogan@gmail.com>
2022-02-08 17:26:45 -0800

    Clean up SpotIdPage and SpotFinderCard

--- frontend/src/components/SpotFinderCard/index.js
+++ frontend/src/components/SpotFinderCard/index.js
@@ -1,36 +1,28 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 
+import styles from "./SpotFinderCard.module.css";
 
-import styles from './SpotFinderCard.module.css'
-
-function SpotFinderCard ({setEditModal, spotId}) {
-  const sessionUser = useSelector(state => state.session.user);
-  const spot = useSelector((state) => state.spots.allSpots[spotId])
-
+function SpotFinderCard({ setEditModal, spotId }) {
+  const sessionUser = useSelector((state) => state.session.user);
+  const spot = useSelector((state) => state.spots.allSpots[spotId]);
 
   return (
-
-  <div className={styles.SpotDivInfoLeft}>
-  <div className={styles.profileImage}></div>
-  {(spot?.userId === sessionUser?.id ?
-    <>
-      <p>You added this spot</p>
-      <button
-        onClick={setEditModal}
-        // onClick={() => setShowEditModal(true)}
-        className={styles.editButton}>
-        Need to make changes?
-      </button>
-    </>
-    :
-    <>
-      <p>Discovered by</p>
-      <p>{spot?.User?.username}</p>
-    </>
-  )}
-</div>
-  )
-
+    <div className={styles.container}>
+      {spot?.userId === sessionUser?.id ? (
+        <>
+          <div className={styles.profileImage}></div>
+          <p>You added this spot</p>
+          <button onClick={setEditModal}>Need to make changes?</button>
+        </>
+      ) : (
+        <>
+          <p>Discovered by:</p>
+          <div className={styles.profileImage}></div>
+          <p>{spot?.User?.username}</p>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default SpotFinderCard;

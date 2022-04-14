695c9bd3c2ffcf4bba6e3ec5f3dc6cf45e249611
Taylor Hogan <taylorb.hogan@gmail.com>
2022-04-14 13:03:27 -0700

    Index redirect for more helpful tabs

---
+++ frontend/src/components/AreaBox/AreaBox.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect, useRef } from "react";
+import parse from "html-react-parser";
+import truncate from "truncate-html";
+import SpotAddModal from "../SpotAddModal"
+import styles from "./AreaBox.module.css";
+
+function AreaBox({ area, selectedArea }) {
+  const [isOpen, setIsOpen] = useState(false);
+  const areaRef = useRef()
+
+  useEffect(() => {
+    if (selectedArea?.id === area.id){
+      areaRef.current.scrollIntoView({behavior: 'smooth'})
+    }
+  },[selectedArea, area])
+
+  return (
+    <div ref={areaRef} className={styles.areaBox} onMouseDown={() => setIsOpen(!isOpen)}>
+      <div className={styles.top}>
+        <div>
+          <h1 className={styles.areaName}>{area.name}</h1>
+          <h2 className={styles.locationDiv}>{area.orgName}</h2>
+        </div>
+        <div>
+          <SpotAddModal selectedArea={area}/>
+        </div>
+      </div>
+      {isOpen ? (
+        <div className={styles.description}>{parse(area.description)}</div>
+      ) : (
+        <div className={styles.description}>
+          {truncate(area.description, 190, { stripTags: true })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default AreaBox;
---
+++ frontend/src/components/AreasList/AreasList.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUsStates } from "../../store/usStates";
+import MapContainer from "../Maps";
+import { searchAreas } from "../../store/areas";
+import AreaBox from "../AreaBox";
+import Dropdown from "../parts/Dropdown";
+import styles from "./AreasList.module.css";
+import MessageNoAreas from "./MessageNoAreas";
+import LoadingContent from "../parts/LoadingContent";
+import ControlPanel from "./ControlPanel";
+import Pagination from "../parts/Pagination/Pagination";
+import Errors from "../parts/Errors";
+
+function AreasList() {
+  const dispatch = useDispatch();
+  const [selectedLocation, setSelectedLocation] = useState(null);
+  const [usStates, setUsStates] = useState([]);
+  const [organization, setOrganization] = useState(null);
+  const [organizations, setOrganizations] = useState([]);
+  const [selectedArea, setSelectedArea] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [resultsPerPage, setResultsPerPage] = useState(25);
+  const [resultPageNum, setResultPageNum] = useState(1);
+  const [errors, setErrors] = useState([]);
+
+  const areas = useSelector((state) =>
+    Object.values(state.areas.searchResults)
+  );
+
+  useEffect(() => {
+    const fetchSelectableLocations = async () => {
+      const usStates = await dispatch(getUsStates());
+      setUsStates(usStates);
+    };
+    fetchSelectableLocations();
+  }, [dispatch]);
+
+  useEffect(() => {
+    const fetchAreas = async () => {
+      const fetch = await dispatch(
+        searchAreas(
+          organization?.id,
+          selectedLocation?.abbreviation,
+          resultsPerPage,
+          resultsPerPage * (resultPageNum - 1)
+        )
+      );
+      if (fetch !== "error") {
+        setIsLoaded(true);
+      }
+    };
+    fetchAreas();
+  }, [dispatch, organization, selectedLocation, resultPageNum]);
+
+  useEffect(() => {
+    const fetchOrganizations = async () => {
+      const res = await fetch(`/api/areas/from-rec-gov/organizations`);
+      const data = await res.json();
+      if (res.ok){
+        const filterOut = [
+          "STATE PARKS",
+          "FEDERAL",
+          "Smithsonian Institution",
+          "Utah",
+          "Maryland",
+          "Texas",
+          "Virginia",
+          "New Mexico",
+          "US Air Force",
+          "Smithsonian Institution Affiliations Program",
+          "",
+        ];
+        const filteredData = data.filter((datum) => {
+          return !filterOut.includes(datum.name);
+        });
+        const obj = {};
+        filteredData.forEach((datum) => {
+          obj[datum.id] = datum;
+        });
+        setOrganizations(obj);
+      } else {
+        console.log(...data.errors);
+        setErrors([...errors, ...data.errors])
+      }
+    };
+    fetchOrganizations();
+  }, []);
+
+  const clearSelection = () => {
+    setSelectedLocation("");
+    setOrganization("");
+  };
+
+  return (
+    <div className={styles.pageContainer}>
+      <div className={styles.pageLeft}>
+        <ControlPanel
+          organizations={organizations}
+          organization={organization}
+          setOrganization={setOrganization}
+          selectedLocation={selectedLocation}
+          usStates={usStates}
+          setSelectedLocation={setSelectedLocation}
+          clearSelection={clearSelection}
+        />
+        {errors.length > 0 && <Errors errors={errors}/> }
+        {isLoaded ? (
+          areas.length > 0 ? (
+            areas.map((area) => (
+              <AreaBox key={area.id} area={area} selectedArea={selectedArea} />
+            ))
+          ) : (
+            <MessageNoAreas />
+          )
+        ) : (
+          <LoadingContent />
+        )}
+        <Pagination
+          isLoaded={isLoaded}
+          resultsPerPage={resultsPerPage}
+          resultPageNum={resultPageNum}
+          setResultPageNum={setResultPageNum}
+        />
+      </div>
+      <div className={styles.pageRight}>
+        <MapContainer pins={areas} zoom={3} setFunction={setSelectedArea} />
+      </div>
+    </div>
+  );
+}
+
+export default AreasList;

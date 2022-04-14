7c25b2db90bd53b4e533a8f27770b02f62c6f7f1
Taylor Hogan <taylorb.hogan@gmail.com>
2022-04-14 15:51:42 -0700

    Index redirect for more helpful tabs

--- frontend/src/components/LoginFormModal/LoginFormModal.js
+++ frontend/src/components/LoginFormModal/LoginFormModal.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Modal } from "../../context/Modal";
 import LoginForm from "./LoginForm";
-import "./../../vars.css";
 import styles from "./LoginFormModal.module.css";
 
 function LoginFormModal() {

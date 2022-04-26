9b0a7c765530f9c5cf582c0f6ef0899725fab0ef
Taylor Hogan <taylorb.hogan@gmail.com>
2022-04-26 16:21:46 -0700

    Implement LoadingContent in community spots

--- frontend/src/components/SpotsList/SpotsList.js
+++ frontend/src/components/SpotsList/SpotsList.js
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getSpots } from '../../store/spots';
-import SpotBox from '../SpotBox';
-import styles from './SpotsList.module.css'
-
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getSpots } from "../../store/spots";
+import SpotBox from "../SpotBox";
+import LoadingContent from "../parts/LoadingContent";
+import styles from "./SpotsList.module.css";
 
 function SpotsList() {
+  const [isLoaded, setIsLoaded] = useState(false);
   const dispatch = useDispatch();
-  const spots = useSelector((state) => Object.values(state.spots.allSpots))
+  const spots = useSelector((state) => Object.values(state.spots.allSpots));
 
   useEffect(() => {
-    dispatch(getSpots())
-  }, [dispatch])
+    const fetchSpots = async () => {
+      const response = await dispatch(getSpots());
+      if (typeof response === "string") {
+        console.log("response", response);
+      } else {
+        setIsLoaded(true);
+      }
+    };
+    fetchSpots();
+  }, [dispatch]);
 
-  return (
+  return isLoaded ? (
     <div className={styles.contentWrapper}>
-      <div className={'contentContainer'}>
-        {spots.map((spot) => <SpotBox key={spot.id} spot={spot} />)}
+      <div className={"contentContainer"}>
+        {spots.map((spot) => (
+          <SpotBox key={spot.id} spot={spot} />
+        ))}
       </div>
     </div>
-  )
+  ) : (
+    <LoadingContent />
+  );
 }
 
 export default SpotsList;

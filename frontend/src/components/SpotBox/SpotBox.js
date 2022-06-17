be462655d9cbe03d52899aced4787035590c1b8f
Taylor Hogan <taylorb.hogan@gmail.com>
2022-06-17 16:55:33 -0700

    Improve responsiveness

--- frontend/src/components/SpotBox/SpotBox.js
+++ frontend/src/components/SpotBox/SpotBox.js
@@ -4,15 +4,11 @@ import styles from "./SpotBox.module.css";
 function SpotBox({ spot }) {
   return (
     <a href={`/spots/${spot.id}`} className={styles.container}>
-      <div className={styles.containerLeft}>
-        <div className={styles.spotImageDiv}>
-          <img
-            className={styles.spotImage}
-            alt={"a glimpse of this spot"}
-            src={spot.SpotImages ? spot.SpotImages[0]?.imgUrl : null}
-          />
-        </div>
-      </div>
+      <img
+        className={styles.spotImage}
+        alt={"a glimpse of this spot"}
+        src={spot.SpotImages ? spot.SpotImages[0]?.imgUrl : null}
+      />
       <div className={styles.containerRight}>
         <h1 className={styles.spotName}>{spot.name}</h1>
         <h2 className={styles.areaName}>{spot.Area?.name}</h2>

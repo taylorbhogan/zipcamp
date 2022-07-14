e0e08c1b816b6e1bb07b8eab5109e1c08c0e002f
Taylor Hogan <taylorb.hogan@gmail.com>
2022-07-14 14:47:27 -0700

    Typo

--- frontend/src/components/AreasList/MessageNoAreas.js
+++ frontend/src/components/AreasList/MessageNoAreas.js
@@ -3,7 +3,7 @@ import styles from './MessageNoAreas.module.css'
 const MessageNoAreas = () => {
   return (
     <div className={styles.message}>
-      <p>There are no Federal pulic lands with that distinction in that location in the rec.gov database.</p>
+      <p>There are no Federal public lands with that distinction in that location in the rec.gov database.</p>
       <p>Try changing your selection or click clear to view all areas.</p>
     </div>
   )

3c67da7c5f9f3c29ecf8f9b49fc49c309465df11
Taylor Hogan <taylorb.hogan@gmail.com>
2022-07-21 18:18:34 -0700

    Implement tip ratings

--- frontend/src/components/TipForm/TipForm.js
+++ frontend/src/components/TipForm/TipForm.js
@@ -19,6 +19,8 @@ function TipForm({ setShowTipForm, tipId }) {
 
   const [errors, setErrors] = useState([]);
   const [tipText, setTipText] = useState(tip ? tip.text : "");
+  const [rating, setRating] = useState(tip ? tip.rating : null);
+  const [hoverRating, setHoverRating] = useState(tip ? tip.rating : null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -32,7 +34,7 @@ function TipForm({ setShowTipForm, tipId }) {
           userId,
           spotId,
           text: tipText,
-          tipRating: 0,
+          rating,
         };
 
         const editedTip = await dispatch(editTip(newTip));
@@ -45,7 +47,7 @@ function TipForm({ setShowTipForm, tipId }) {
           userId,
           spotId,
           text: tipText,
-          tipRating: 0,
+          rating,
         };
 
         const createdTip = await dispatch(createTip(newTip));
@@ -68,6 +70,88 @@ function TipForm({ setShowTipForm, tipId }) {
       <form className="form" onSubmit={handleSubmit}>
         <h1 className={"formHeader"}>add a tip</h1>
         {errors.length > 0 && <Errors errors={errors} />}
+        <div className={styles.buttons}>
+          <button
+            style={
+              rating >= 1
+                ? { color: "var(--green1)", transform: "var(--starScale)" }
+                : hoverRating >= 1
+                ? { transform: "var(--starScale)" }
+                : null
+            }
+            type="button"
+            className="material-icons"
+            onClick={(e) => setRating(e.target.dataset.rating)}
+            onMouseEnter={(e) => setHoverRating(e.target.dataset.rating)}
+            data-rating={1}
+          >
+            grade
+          </button>
+          <button
+            style={
+              rating >= 2
+                ? { color: "var(--green1)", transform: "var(--starScale)" }
+                : hoverRating >= 2
+                ? { transform: "var(--starScale)" }
+                : null
+            }
+            type="button"
+            className="material-icons"
+            onClick={(e) => setRating(e.target.dataset.rating)}
+            onMouseEnter={(e) => setHoverRating(e.target.dataset.rating)}
+            data-rating={2}
+          >
+            grade
+          </button>
+          <button
+            style={
+              rating >= 3
+                ? { color: "var(--green1)", transform: "var(--starScale)" }
+                : hoverRating >= 3
+                ? { transform: "var(--starScale)" }
+                : null
+            }
+            type="button"
+            className="material-icons"
+            onClick={(e) => setRating(e.target.dataset.rating)}
+            onMouseEnter={(e) => setHoverRating(e.target.dataset.rating)}
+            data-rating={3}
+          >
+            grade
+          </button>
+          <button
+            style={
+              rating >= 4
+                ? { color: "var(--green1)", transform: "var(--starScale)" }
+                : hoverRating >= 4
+                ? { transform: "var(--starScale)" }
+                : null
+            }
+            type="button"
+            className="material-icons"
+            onClick={(e) => setRating(e.target.dataset.rating)}
+            onMouseEnter={(e) => setHoverRating(e.target.dataset.rating)}
+            data-rating={4}
+          >
+            grade
+          </button>
+          <button
+            style={
+              rating >= 5
+                ? { color: "var(--green1)", transform: "var(--starScale)" }
+                : hoverRating >= 5
+                ? { transform: "var(--starScale)" }
+                : null
+            }
+            type="button"
+            className="material-icons"
+            onClick={(e) => setRating(e.target.dataset.rating)}
+            onMouseEnter={(e) => setHoverRating(e.target.dataset.rating)}
+            data-rating={5}
+          >
+            grade
+          </button>
+        </div>
         <Input
           type="textarea"
           value={tipText}

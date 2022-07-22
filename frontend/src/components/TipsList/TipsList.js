b54eb8858d890cfb381f8aee601625b083a0c77e
Taylor Hogan <taylorb.hogan@gmail.com>
2022-07-22 12:57:34 -0700

    Implement average ratings

--- frontend/src/components/TipsList/TipsList.js
+++ frontend/src/components/TipsList/TipsList.js
@@ -1,31 +1,15 @@
-import React, { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-import { getTips } from "../../store/tips";
-
-import styles from "./TipsList.module.css";
+import React from "react";
 import TipBox from "../TipBox";
 import TipButton from "../TipButton";
+import styles from "./TipsList.module.css";
 
-function TipsList({ spot }) {
-  const dispatch = useDispatch();
-  const { spotId } = useParams();
-
-  const tips = useSelector((state) => Object.values(state.tips.allTips));
-  const thisSpotTips = tips.filter((tip) => {
-    return tip.spotId === +spotId;
-  });
-
-  useEffect(() => {
-    dispatch(getTips());
-  }, [dispatch]);
-
+function TipsList({ spot, tips }) {
   return (
     <div className={styles.container}>
-      <TipButton spot={spot}/>
-      {thisSpotTips.length > 0 && (
+      <TipButton spot={spot} />
+      {tips.length > 0 && (
         <div className={styles.tips}>
-          {thisSpotTips.map((tip) => (
+          {tips.map((tip) => (
             <TipBox key={tip.id} tip={tip} />
           ))}
         </div>

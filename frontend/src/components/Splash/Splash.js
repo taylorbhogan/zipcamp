67ab51e0366d88141b65b3dfbc6f06489bae5c54
Taylor Hogan <taylorb.hogan@gmail.com>
2022-07-26 13:25:40 -0700

    Update Splash

--- frontend/src/components/Splash/Splash.js
+++ frontend/src/components/Splash/Splash.js
@@ -11,27 +11,27 @@ function Splash() {
         blurb={
           "browse the nation's public lands as delivered by the rec.gov API"
         }
-        imgSrc={"/images/defaults/undraw_explore.svg"}
+        imgSrc={"/images/defaults/explore.gif"}
       />
       <SplashSection
         title={"x marks the spot"}
         blurb={
           "make your mark while leaving no trace. save any spot you find so you or anyone in the community can find your way back later"
         }
-        imgSrc={"/images/defaults/undraw_mark.svg"}
+        imgSrc={"/images/defaults/mark.gif"}
         flip={true}
       />
       <SplashSection
         title={"add tips"}
-        blurb={"make notes about your spot and save ideas for next time"}
-        imgSrc={"/images/defaults/undraw_tips.svg"}
+        blurb={"make notes about your spot, leave a rating, and save ideas for next time"}
+        imgSrc={"/images/defaults/tips.gif"}
       />
       <SplashSection
         title={"capture memories"}
         blurb={
           "upload photos of your spot to preserve your memories - and find your way back!"
         }
-        imgSrc={"/images/defaults/undraw_photos.svg"}
+        imgSrc={"/images/defaults/photos.gif"}
         flip={true}
       />
     </div>

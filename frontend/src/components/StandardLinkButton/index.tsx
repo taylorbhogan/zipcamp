5cf3e8ed79b086df457d9420ece7f58952f72231
Taylor Hogan <taylorb.hogan@gmail.com>
2025-01-09 14:36:48 -0800

    Convert StandardLinkButton to typescript

--- frontend/src/components/StandardLinkButton/index.tsx
+++ frontend/src/components/StandardLinkButton/index.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "./StandardLinkButton.module.css";
 
-function StandardLinkButton({ buttonText, href }) {
+interface StandardLinkButtonProps {
+  buttonText: string;
+  href: string;
+}
+
+const StandardLinkButton: React.FC<StandardLinkButtonProps> = ({ buttonText, href }) => {
   return (
     <Link className={styles.standardLinkButton} to={href}>
       {buttonText}
     </Link>
   );
-}
+};
 
 export default StandardLinkButton;

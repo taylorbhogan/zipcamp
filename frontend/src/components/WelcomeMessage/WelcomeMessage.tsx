db80fe9455b67bb3adcd8c27e2ad2b254d1f43b9
Taylor Hogan <taylorb.hogan@gmail.com>
2025-01-09 14:47:21 -0800

    Convert WelcomeMessage to typescript

--- frontend/src/components/WelcomeMessage/WelcomeMessage.tsx
+++ frontend/src/components/WelcomeMessage/WelcomeMessage.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import StandardLinkButton from "./../StandardLinkButton";
+import StandardLinkButton from "../StandardLinkButton";
 import styles from "./WelcomeMessage.module.css";
 import "./../../index.css";
 
-function WelcomeMessage() {
+const WelcomeMessage: React.FC = () => {
   const buttonText = "find your way";
   const buttonHref = "/areas";
+
   return (
     <div className={styles.container}>
       <div className={styles.welcomeMessage}>
         <p>
           the clearest way into the universe is through a forest wilderness.
         </p>
-        <p>expore public lands and save the cool spots you discover.</p>
+        <p>explore public lands and save the cool spots you discover.</p>
         <div className={styles.buttonWrapper}>
           <StandardLinkButton buttonText={buttonText} href={buttonHref} />
         </div>
@@ -23,6 +24,6 @@ function WelcomeMessage() {
       />
     </div>
   );
-}
+};
 
 export default WelcomeMessage;
